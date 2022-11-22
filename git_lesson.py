c1fad6a22b651cef72045ff442308cd4db9758af
Shane029 <artyom.grigoryan.@gmail.com>
2022-11-21 21:41:57 -0800

    Annogramm

--- git_lesson.py
+++ git_lesson.py
@@ -1,1 +1,11 @@
-print("hello world")
+word1 = input('Enter the first word:   ')
+word2 = input('Enter the second word:   ')
+annogramm1 = {}
+annogramm2 = {}
+count = 1
+for i in word1:
+    annogramm1[i] = count
+for j in word2:
+    annogramm2[j] = count
+      
+print(annogramm1 == annogramm2 and len(word1) == len(word2))
