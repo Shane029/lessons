b3214e12358494277c8d29508e57543ffa069614
Shane029 <artyom.grigoryan.@gmail.com>
2022-11-21 21:46:28 -0800

    Long Annogramm

--- annogramm.py
+++ annogramm.py
@@ -1,11 +1,15 @@
-word1 = input('Enter the first word:   ')
-word2 = input('Enter the second word:   ')
+word1 = input('Enter the first word:   ').lower()
+word2 = input('Enter the second word:   ').lower()
 annogramm1 = {}
 annogramm2 = {}
 count = 1
 for i in word1:
+    if i == ' ':
+        continue
     annogramm1[i] = count
 for j in word2:
+    if j == ' ':
+        continue
     annogramm2[j] = count
       
 print(annogramm1 == annogramm2 and len(word1) == len(word2))
