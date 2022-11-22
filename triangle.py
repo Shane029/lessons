ea60a44186f9eb0c381488cf403673591587c963
Shane029 <artyom.grigoryan.@gmail.com>
2022-11-21 21:15:42 -0800

    Triangle

---
+++ triangle.py
@@ -0,0 +1,37 @@
+count = int(input('Enter the amount of numbers in the triangle:      '))
+x = 1
+tiv = 1
+quant = 0
+sum = 1
+f = 0
+while sum <= count:
+    x = x + 1
+    sum = sum + x
+    quant = quant + 1
+print (f'The triangle is  {quant} X {quant}')
+
+for i in range(quant ):
+    
+    print( tiv, end = ' ')
+    
+    for j in range(0, i ):
+        
+        f = f +  tiv + quant - j -1
+        if tiv < 10:
+            print(f' {f}', end = ' ')
+        else:
+            print(f'{f} ', end = ' ')
+        f = f - tiv
+        
+    f = 0
+    tiv += 1
+    print()
+print()
+
+# 1
+# 2 6
+# 3 7 10 
+# 4 8 11 13
+# 5 9 12 14 15
+
+
